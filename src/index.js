39d060d07118ec16915fdf4c022ca10d2391fc5d
Aravind A <aravindarali125@gmail.com>
2020-06-19 17:59:34 +0530

    Initialised chat-app project

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+
+const publicDirectoryPath = path.join(__dirname,"../public");
+
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection",()=>{
+    console.log("New webSocket Connection");
+});
+
+server.listen(port,()=>{
+    console.log("Server started on port "+port);
+})
